d98add6b4c10147dcf49ecee13f88efa0c05c2c0
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-08 15:24:48 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,4 +2,15 @@
 
 class HauntedHouse < ActiveRecord::Migration[4.2]
 
+  def create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.string :theme
+    t.float :price
+    t.boolean :family_friendly
+    t.     :opening_date
+    t.      :closing_date
+    t.string :description 
+  
+  end 
 end
