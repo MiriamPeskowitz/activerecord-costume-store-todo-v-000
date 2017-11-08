5ce54e6a4ce6ecc8c43922a79b19ae23eb97a9c0
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-08 15:16:44 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,9 +6,9 @@ class CostumeStore < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :num_of_employees 
+      t.integer :num_of_employees
       t.boolean :still_in_business
       t.      :opening_time
       t.     :closing_time
-  end 
+  end
 end
